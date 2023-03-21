1b35692823b36f4f862415e34d1530365dc79e4e
Dainis Berzins <db5331@gmail.com>
2023-03-21 21:33:24 +0200

    Added get_auth_token view

--- dati/views.py
+++ dati/views.py
@@ -32,6 +32,25 @@ def index(request):
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
 
+from django.contrib.auth import authenticate
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+from rest_framework.authtoken.models import Token
+
+@csrf_exempt
+def get_auth_token(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            token, created = Token.objects.get_or_create(user=user)
+            return JsonResponse({'token': token.key})
+        else:
+            return JsonResponse({'error': 'Invalid credentials'}, status=400)
+    else:
+        return JsonResponse({'error': 'Invalid request method'}, status=400)
+
 from django.contrib.auth import authenticate, login
 from django.http import JsonResponse
 
