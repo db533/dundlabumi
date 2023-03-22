b47700b63ae5252c4fb3016cb319599f3e23c4e7
Dainis Berzins <db5331@gmail.com>
2023-03-22 13:40:24 +0200

    Added get_auth_token view

--- stats/settings.py
+++ stats/settings.py
@@ -55,7 +55,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'dati.apps.DatiConfig',
-    'authtoken',
+    'rest_framework.authtoken',
     'rest_framework',
 ]
 
