2587e1e9863e60645686c579e0ea655ffbfad593
Dainis Berzins <db5331@gmail.com>
2023-03-24 20:46:03 +0200

    Adding form and view to show user details in website.

--- stats/urls.py
+++ stats/urls.py
@@ -46,5 +46,7 @@ urlpatterns += [
       path('login', login_view, name='login_view'),
       path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
       path('get-auth-token/', get_auth_token, name='get_auth_token'),
+      path('user-details/', user_data, name='user_details'),
+
 ]
 
