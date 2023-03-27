15f267b1c461420fb9c4f6e4745f29cf33df162f
Dainis Berzins <db5331@gmail.com>
2023-03-27 10:54:01 +0300

    Fixing UserForm

--- dati/forms.py
+++ dati/forms.py
@@ -1,6 +1,6 @@
 from django import forms
+from django.core.validators import MinValueValidator
 from .models import UserModel
 
 class UserForm(forms.Form):
-    user_id = forms.IntegerField(label='User ID')
-
+    user_id = forms.IntegerField(label='User ID', validators=[MinValueValidator(1)])
