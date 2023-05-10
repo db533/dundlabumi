51f56bad307a89440547af56a332fbb41fbcd4aa
Dainis Berzins <db5331@gmail.com>
2023-05-10 12:53:21 +0300

    Changing link API to user get_session_and_usermodel(request) function to track session and usermodel

--- dati/views.py
+++ dati/views.py
@@ -411,7 +411,7 @@ def page(request, id):
 from django.shortcuts import redirect
 
 def link(request, id):
-    temp_message=""
+    temp_message = ""
     # Read data about the link that brought the user to the site.
     if not Redirect.objects.filter(redirect_code=id).exists():
         # The redirect code does not exist in the database. Push user to the shop page instead.
@@ -424,99 +424,9 @@ def link(request, id):
         target_url = redirect_record.target_url
         wpid_of_linked_page = redirect_record.wpid_id
 
-        # Get the session from the received request
-        if 's_key' in request.session:
-            # A session key is stored in s_key.
-            session_key = request.session['s_key']
-            temp_message += "s_key present in request.session. "
-            # Change the session key to the store value from the cookie.
-            #request.session.session_key = session_key
-            #request.session.save()
-        if not 's_key' in request.session or session_key == None:
-            temp_message += "s_key missing or None. "
-            request.session.create()
-            request.session.save()
-            session_key = request.session.session_key
-            # Save the session to s_key
-            request.session['s_key'] = session_key
-            request.session.save()
-        if session_key == None:
-            temp_message += "s_key still None. "
-        if Session.objects.filter(session_key=session_key).exists():
-            session = Session.objects.get(session_key=session_key)
-        else:
-            session = Session.objects.create(session_key=session_key)
-
-        # Get the usermodel for this session. Create if it is not associated.
-        # First get a queryset of the usermodels that have this session_key
-        usermodels_for_session = session.usermodels.all()
-        if len(usermodels_for_session) > 0:
-            # A usermodel instance was found for the current session key.
-            usermodel = UserModel.objects.get(sessions=session)
-            #saved_username = usermodel.username
-            #saved_email = usermodel.email
-            temp_message += " retrieved usermodel. "
-        elif redirect_usermodel is not None :
-            # Session is new, but is apparently for an existing username.
-            usermodel = redirect_usermodel
-            usermodel.sessions.add(session)
-            usermodel.save()
-            temp_message += " New session, but username known. "
-        else:
-            # No usermodel exists for this session key. Create one and add the current session.
-            usermodel = UserModel.objects.create()
-            usermodel.sessions.add(session)
-            usermodel.save()
-            temp_message += " created usermodel "
-            #saved_username = ""
-            #saved_email = ""
-
-        # Check for a logged in user.
-        session_data = session.get_decoded()
-        uid = session_data.get('_auth_user_id')
-        if uid is not None:
-            user = User.objects.get(id=uid)
-            username = user.username
-            temp_message += " username = " + str(username)
-            user_email = user.email
-            temp_message += " user_email = " + str(user_email)
-        else:
-            # User not logged in.
-            username = ""
-
-        # If a username is known, check it is recorded in UserModel.
-        if uid is not None:
-            # A user is logged in.
-            #if usermodel is not None:
-            # the redirect link referred to a specific user.
-            if usermodel.username is None or usermodel.username == "" or usermodel.username == "saknesar_stats_dev":
-                # The username of the specific user is not yet stored in the database.
-                usermodel.username=username
-                usermodel.save()
-                temp_message += " Added username to UserModel"
-            #else:
-                # User logged in, but the redirect link did not refer to a specific user.
-                # See if a user already exists for this email.
-            #    if user_email is not None:
-            #        if UserModel.objects.filter(email=user_email).exists():
-            #            usermodel = UserModel.objects.get(email=user_email)
-            #            if usermodel.username is None:
-            #                # The username of the specific user is not yet stored in the database.
-            #                usermodel.username = username
-            #                usermodel.save()
-            #                temp_message += " Setting username to UserModel when redirect had no email."
-            #        else:
-            #            # User is logged in, but is not a subscriber. Create a UserModel for this user.
-            #            usermodel = UserModel.objects.create(email=user_email, username=username, session=session)
-            #            temp_message += " Created new user for a new logged in user that is not a subscriber."
-
-        #temp_message += " usermodel = " + str(usermodel)
-        # Connect the current session to this usermodel, if not already added.
-
-        if usermodel.sessions != session:
-            usermodel.sessions.add(session)
-            usermodel.save()
-            temp_message += " Session linked to user."
+        LogEntry.objects.create(key='Link click occured. Redirect code:', value=id)
+        session, usermodel = get_session_and_usermodel(request)
+        print('usermodel.id:', usermodel.id)
 
         # Create the response to return to the user.
         response = redirect(target_url)
