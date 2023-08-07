d3dc4c9a48676aa6df2048abd82f5b29dbd1b4e4
Dainis Berzins <db5331@gmail.com>
2023-08-07 09:32:20 +0300

    Added allowed host www.

--- stats/settings.py
+++ stats/settings.py
@@ -31,6 +31,7 @@ TEMPLATE_DEBUG = DEBUG
 
 ALLOWED_HOSTS = env('allowed_hosts', cast=[str])
 ALLOWED_HOSTS.append("www."+ALLOWED_HOSTS[0])
+ALLOWED_HOSTS.append("dundlabumi.lv")
 print('ALLOWED_HOSTS:',ALLOWED_HOSTS)
 
 # Get the IP address of this host
