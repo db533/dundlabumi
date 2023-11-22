25cba30cf47e52431ad8334c22a8c7ecb5e0832f
Dainis Berzins <db5331@gmail.com>
2023-11-22 22:22:54 +0200

    Pushing store-feedback plugin code

--- stats/store-feedback.php
+++ stats/store-feedback.php
@@ -11,37 +11,60 @@
 
 // Function to save the feedback
 function store_feedback_save() {
-    // Similar to previous save function
-    // ...
+    if (is_user_logged_in()) {
+        $user_id = get_current_user_id();
+
+        // Check if the form is submitted
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // Sanitize and store the feedback data
+            $rating = intval($_POST['rating']);
+            $recommend = sanitize_text_field($_POST['recommend']);
+            $review = sanitize_textarea_field($_POST['review']);
+
+            update_user_meta($user_id, 'store_rating', $rating);
+            update_user_meta($user_id, 'store_recommend', $recommend);
+            update_user_meta($user_id, 'store_review', $review);
+
+            // Optional: Send an email notification or perform other actions
+        }
+    }
 }
 
 // Shortcode to display the feedback form
 function store_feedback_form_shortcode() {
-    // Similar to previous form shortcode
-    // ...
-}
-
-// Function to calculate and display the summary of reviews
-function store_feedback_summary() {
-    $users = get_users();
-    $total_rating = $total_recommend = $count = 0;
-
-    foreach ($users as $user) {
-        $rating = get_user_meta($user->ID, 'store_rating', true);
-        $recommend = get_user_meta($user->ID, 'store_recommend', true);
-
-        if ($rating) {
-            $total_rating += $rating;
-            $total_recommend += ($recommend === 'yes' ? 1 : 0);
-            $count++;
+    ob_start();
+    if (is_user_logged_in()) {
+        $user_id = get_current_user_id();
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            store_feedback_save();
+            echo '<meta http-equiv="refresh" content="0">';
         }
+        ?>
+        <form method="post">
+            <h3>Rate Our Store</h3>
+            <div>
+                <!-- Star rating input -->
+                <?php for ($i = 1; $i <= 5; $i++): ?>
+                    <input type="radio" name="rating" value="<?php echo $i; ?>"> <?php echo $i; ?>
+                <?php endfor; ?>
+            </div>
+            <h3>Would you recommend our store to friends?</h3>
+            <div>
+                <label><input type="radio" name="recommend" value="yes"> Yes</label>
+                <label><input type="radio" name="recommend" value="no"> No</label>
+            </div>
+            <h3>Review</h3>
+            <textarea name="review"></textarea>
+            <div>
+                <button type="submit">Submit Feedback</button>
+            </div>
+        </form>
+        <?php
+    } else {
+        echo '<p>Please log in to provide feedback.</p>';
+        // Optional: Display a login form or link
     }
-
-    $average_rating = $count > 0 ? round($total_rating / $count, 1) : 0;
-    $recommend_percentage = $count > 0 ? round(($total_recommend / $count) * 100) : 0;
-
-    echo "<p>Average Rating: $average_rating / 5</p>";
-    echo "<p>Recommend: $recommend_percentage% of reviewers recommend this store</p>";
+    return ob_get_clean();
 }
 
 // Function to display individual reviews
