dc24a9215365c1e74e783945182e264a04a0f25d
Dainis Berzins <db5331@gmail.com>
2023-11-22 22:25:44 +0200

    Pushing store-feedback plugin code

--- stats/store-feedback.php
+++ stats/store-feedback.php
@@ -124,40 +124,6 @@ function store_feedback_summary() {
     echo "<p>Recommend: $recommend_percentage% of reviewers recommend this store</p>";
 }
 
-// Function to display individual reviews
-function store_feedback_reviews($sort = 'rating') {
-    $args = array(
-        'meta_query' => array(
-            'relation' => 'AND',
-            array(
-                'key'     => 'store_rating',
-                'compare' => 'EXISTS',
-            ),
-            array(
-                'key'     => 'store_review',
-                'compare' => 'EXISTS',
-            ),
-        ),
-        'orderby' => $sort === 'rating' ? 'meta_value_num' : 'registered',
-        'order'   => 'DESC',
-        'meta_key' => 'store_rating',
-    );
-    $users = get_users($args);
-
-    foreach ($users as $user) {
-        $rating = get_user_meta($user->ID, 'store_rating', true);
-        $review = get_user_meta($user->ID, 'store_review', true);
-        $recommend = get_user_meta($user->ID, 'store_recommend', true);
-        $thumbs_icon = $recommend === 'yes' ? 'thumbs-up.png' : 'thumbs-down.png'; // Replace with actual image URLs
-
-        echo "<div class='review'>";
-        echo "<p>Rating: $rating / 5</p>";
-        echo "<p>Review: $review</p>";
-        echo "<img src='$thumbs_icon' alt='$recommend'>";
-        echo "</div>";
-    }
-}
-
 // Shortcode to display the summary and reviews
 function store_feedback_display_shortcode($atts) {
     ob_start();
